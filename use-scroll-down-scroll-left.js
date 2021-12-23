84385d8a835181dd546c714333d6c017578cfac8
Mehul Singh Teya <49727032+daxter-army@users.noreply.github.com>
2021-12-23 23:18:56 +0530

    Create use-scroll-down-scroll-left.js

---
+++ use-scroll-down-scroll-left.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+const useScrollDownToScrollLeft = (scrollerRef) => {
+  const [scrollPos, setScrollPos] = useState(0);
+
+  useEffect(() => {
+    scrollerRef.current.addEventListener("wheel", (event) => {
+      const scrollCurrPos = scrollerRef.current.scrollLeft;
+      scrollerRef.current.scrollLeft = scrollCurrPos + event.deltaY;
+      setScrollPos(scrollCurrPos);
+    });
+  }, [scrollerRef]);
+
+  return scrollPos;
+};
+
+export default useScrollDownToScrollLeft;
